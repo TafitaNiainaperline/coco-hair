b359b239a962e30e1351f5544b362cb814a46c9a
TafitaNiainaperline <tafitaniainaperline@gmail.com>
2026-03-23 13:01:50 +0300

    Add MVola phone number back to payment section

--- js/script.js
+++ js/script.js
@@ -229,6 +229,7 @@ function checkout() {
             <div class="payment-methods">
               <div class="payment-method">
                 <span class="method-label">MVola</span>
+                <span class="method-value">+261 34 70 20 583</span>
               </div>
             </div>
           </div>
@@ -397,7 +398,7 @@ Votre commande #${order.id} a été confirmée.
 
 TOTAL: ${order.total.toLocaleString()} Ar
 
-Veuillez envoyer le paiement via MVola
+Veuillez envoyer le paiement via MVola au: +261 34 70 20 583
 
 Après paiement, votre commande sera livrée ${deliveryLabel.toLowerCase()}.
 
